b7033da837ad6208630226fe18952b5ce99bbae2
wookiemikes <mikhaelp32@gmail.com>
2020-04-03 11:41:00 +0800

    yay

--- register.php
+++ register.php
@@ -54,7 +54,7 @@ include 'server/serverconnection.php';
     <!-- Header Section Begin -->
     <header class="header-section">
         <div class="header-top">
-        <div class="container">
+            <div class="container">
                 <div class="ht-left">
                     <div class="mail-service">
                         <div id="clockbox" style="font-size: 14px;"></div>
@@ -99,35 +99,64 @@ include 'server/serverconnection.php';
             </div>
         </div>
         <div class="container">
-           <div class="inner-header">
+            <div class="inner-header">
                 <div class="row">
                     <div class="col-lg-2 col-md-2">
                         <div class="logo">
-                            <a href="index.php">
+                            <a href="./index.html">
                                 <img src="assets/img/glogo.jpg" alt="">
                             </a>
                         </div>
                     </div>
-                    <div class="mt-5 col-lg-7 col-md-7">
+                    <div class="col-lg-7 col-md-7 mt-4">
                         <div class="advanced-search">
-                            <button type="button" class="category-btn">Search</button>
+                            <button type="button" class="category-btn">Search Store</button>
                             <div class="input-group">
                                 <input type="text" placeholder="What do you need?">
-                                <button type="button" class=""><i class="ti-search"></i></button>
+                                <button type="button"  class=""><i class="ti-search"></i></button>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-3 text-right col-md-3 mt-5">
+                    <div class="col-lg-3 text-right col-md-3 mt-4">
                         <ul class="nav-right">
-                            <li class="cart-icon">
-                                <a href="#">
+                            <li class="cart-icon"><a href="#">
                                     <i class="icon_bag_alt"></i>
-                                    <span></span>
+                                    <span>3</span>
                                 </a>
                                 <div class="cart-hover">
+                                    <div class="select-items">
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="si-pic"><img src="img/select-product-1.jpg" alt=""></td>
+                                                    <td class="si-text">
+                                                        <div class="product-selected">
+                                                            <p>$60.00 x 1</p>
+                                                            <h6>Kabino Bedside Table</h6>
+                                                        </div>
+                                                    </td>
+                                                    <td class="si-close">
+                                                        <i class="ti-close"></i>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="si-pic"><img src="img/select-product-2.jpg" alt=""></td>
+                                                    <td class="si-text">
+                                                        <div class="product-selected">
+                                                            <p>$60.00 x 1</p>
+                                                            <h6>Kabino Bedside Table</h6>
+                                                        </div>
+                                                    </td>
+                                                    <td class="si-close">
+                                                        <i class="ti-close"></i>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5></h5>
+                                        <h5>$120.00</h5>
                                     </div>
                                     <div class="select-button">
                                         <a href="#" class="primary-btn view-card">VIEW CARD</a>
@@ -135,13 +164,12 @@ include 'server/serverconnection.php';
                                     </div>
                                 </div>
                             </li>
-                            <li class="cart-price">₱ 00.00</li>
+                            <li class="cart-price">$150.00</li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-
     </header>
     <!-- Header End -->
 
