3fed7bdc30b0ac8c8c4a59f4484b9564f4ee3b70
wookiemikes <mikhaelp32@gmail.com>
2020-05-22 08:46:06 +0800

    logout good

--- shop.php
+++ shop.php
@@ -611,6 +611,28 @@
             </div>
         </div>
     </div>
+    <!--Logout Modal -->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Logout</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure?
+                </div>
+                <form method="post" action="index.php">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Nevermind</button>
+                        <button type="submit" name="logout" class="btn btn-primary">Yes, Let me go</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
     <!-- Footer Section End -->
 
     <!-- Js Plugins -->
